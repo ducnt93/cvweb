133fbd4ed3f4b5ab3dffde472474dfa46288ca7d
dovanhungcis <dovanhungci@gmail.com>
2016-12-23 08:45:19 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,16 @@ Route::get('/ajax/{idTheLoai}', ['as' => 'user.search', 'uses' => 'SearchControl
 Route::get('/ajaxtc/{idTheLoai}', ['as' => 'user.searchtc', 'uses' => 'SearchController@searchtc']);
 Route::get('html_email/{ppto}', ['as' => 'user.html_email', 'uses' => 'MailController@html_email']);
 Route::get('exp/{mail}', ['as' => 'user.xuatfile', 'uses' => 'UserController@xuatfile']);
+
+Route::get('login',['as'=>'getLogin','uses'=>"admin\adminController@getLogin"]);
+Route::post('login',['as'=>'getLogin','uses'=>"admin\adminController@postLogin"]);
+Route::get('logout',['as'=>'getLogout','uses'=>'admin\adminController@getLogout']);
+
+Route::group(['middleware'=>'auth'],function(){
+    Route::group(['prefix'=>'dvh_admin'],function(){
+       Route::get('/',function(){
+          return view('admin.index');
+       });
+    });
+});
+
